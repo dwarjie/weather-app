c87482a698a5fb70a15f0d9c73acb1b7755357d4
unknown <macoycoder@gmail.com>
2022-04-15 01:09:17 -0700

    Extra weather info now working

--- src/components/SideInfo.js
+++ src/components/SideInfo.js
@@ -4,14 +4,23 @@ import React from "react";
 import SideInfoComponent from "./SideInfoComponent";
 
 const SideInfo = (props) => {
+	const meterToKilometer = () => {
+		return Math.round(props.currentWeather.windSpeed * (18 / 5));
+	};
 	return (
 		<div className="text-right sm: w-1/2 mt-12">
 			<SideInfoComponent
 				title="Humidity"
 				value={props.currentWeather.humidity}
 			/>
-			<SideInfoComponent title="Chance of Rain" value="0%" />
-			<SideInfoComponent title="Wind Speed" value="1.4km/h" />
+			<SideInfoComponent
+				title="Chance of Rain"
+				value={`${props.dailyWeather.rainChance}%`}
+			/>
+			<SideInfoComponent
+				title="Wind Speed"
+				value={`${meterToKilometer()} Km/h`}
+			/>
 		</div>
 	);
 };
