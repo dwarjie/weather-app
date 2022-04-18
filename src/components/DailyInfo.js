10d96b9288e2164148e2ae798d9272ccd3c3d8c3
unknown <macoycoder@gmail.com>
2022-04-17 21:40:57 -0700

    Refactored using maps() in showing daily weather components

--- src/components/DailyInfo.js
+++ src/components/DailyInfo.js
@@ -14,24 +14,22 @@ const DailyInfo = (props) => {
 		"Saturday",
 	];
 	const displayDaily = () => {
-		let currDay = 1;
-		let dayComponent = props.dailyWeather.daily.map((data) => {
-			return <DailyInfoComponent day={arrDays[currDay]} temp={data.temp.max} />;
+		return props.dailyWeather.daily.map((data, index) => {
+			return <DailyInfoComponent day={arrDays[index]} temp={data.temp.max} />;
 		});
-
-		return dayComponent;
 	};
+
 	return (
 		<div className="w-full sm: mt-16">
 			<ul>
-				{/* {displayDaily} */}
-				<DailyInfoComponent day="Sunday" />
+				{displayDaily()}
+				{/* <DailyInfoComponent day="Sunday" />
 				<DailyInfoComponent day="Monday" />
 				<DailyInfoComponent day="Tuesday" />
 				<DailyInfoComponent day="Wednesday" />
 				<DailyInfoComponent day="Thursday" />
 				<DailyInfoComponent day="Friday" />
-				<DailyInfoComponent day="Saturday" />
+				<DailyInfoComponent day="Saturday" /> */}
 			</ul>
 		</div>
 	);
